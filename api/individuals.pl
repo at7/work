99d021fca40339c74bdef46fd641c1b0c2e0c2af
Anja Thormann <anja@ebi.ac.uk>
2015-04-29 16:59:29 +0100

    add tests for new schema

--- api/individuals.pl
+++ api/individuals.pl
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 
 require "../utils/db.pl";
-
+use Bio::EnsEMBL::Registry;
 use Getopt::Long;
 
 # perl individuals.pl -db_config db_config
@@ -12,25 +12,52 @@ my $config = {};
 GetOptions(
   $config,
   'db_config=s',
+  'registry=s',
   'mode=s',
 ) or die "Error: Failed to parse command line arguments\n";
 
-die ('A db_config file is required (--db_config)') unless (defined($config->{db_config}));
-
-if ($config->{mode} eq 'create_samples') {
-  create_samples();
-}
-
+create_samples() if ($config->{mode} eq 'create_samples');
+test_new_schema() if ($config->{mode} eq 'test_new_schema');
 
 sub test_new_schema {
+  die ('A registry file is required (--registry)') unless (defined($config->{registry}));
+  my $registry_file = $config->{registry};
+  my $registry = 'Bio::EnsEMBL::Registry';
+  $registry->load_all($registry_file);
 
+  my $ia = $registry->get_adaptor('rat', 'variation', 'individual'); 
+  my $individuals = $ia->fetch_all();
+  # fetch_all_Samples_by_Individual($individual);
+  # 
 
+  foreach my $i (@$individuals) {
+    print $i->name, "\n";
+  }
+=begin
+  Create a new study:
+  my $study_wgs = Bio::EnsEMBL::Variation::Study->new(-name => 'WGS');
+  my $study_exome = Bio::EnsEMBL::Variation::Study->new(-name => 'Exome'); 
+  my $individual = $individual_adaptor->fetch_by_name('NA12345'); 
+  my $first_sample = Bio::EnsEMBL::Variation::Individual->new(
+                      -individual => $individual,
+                      -study => $study_wgs,
+                      -is_sample => 1,  
+                     );
+  $individual_adaptor->store($first_sample);
+  ...  
+
+  Genotypes
+  my $individual_genotypes = $indivdiual_genotype_adaptor->fetch_all_by_Variation($variation);
+  my $individual_genotypes = $indivdiual_genotype_adaptor->fetch_all_by_Variation($variation, $study);
+
+=end
+=cut
 }
-
 
 sub create_samples {
   my $individuals = {};
   my $is_sample = {};
+  die ('A db_config file is required (--db_config)') unless (defined($config->{db_config}));
   my $dbh = get_dbh($config->{db_config});  
   my $stmt = qq{SELECT individual_id, name, description FROM individual;};
 
