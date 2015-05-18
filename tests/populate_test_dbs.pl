bde83e37bbb0f83f9eff0614a7def5f34f1f1900
Anja Thormann <anja@ebi.ac.uk>
2015-05-18 11:29:29 +0100

    populate test dbs

---
+++ tests/populate_test_dbs.pl
@@ -0,0 +1,4 @@
+use Bio::EnsEMBL::Test::MultiTestDB;
+my $path = '/Users/anjathormann/Documents/DEV/ensembl-variation/modules/t';
+my $species = 'homo_sapiens';
+my $test = Bio::EnsEMBL::Test::MultiTestDB->new($species, $path);
