de2a27013ae534b2fb0ba89bf20843e104fce6e2
Anja Thormann <anja@ebi.ac.uk>
2016-02-02 13:35:31 +0000

    ld computation speed up tests

--- api/ld.pl
+++ api/ld.pl
@@ -8,22 +8,22 @@ my $registry = 'Bio::EnsEMBL::Registry';
 $registry->load_registry_from_db(
   -host => 'ensembldb.ensembl.org',
   -user => 'anonymous',
-  -db_version => 83,
+#  -db_version => 83,
 );
 
 my $registry_file = 'ensembl.registry';
-my $species = 'human';
+my $species = 'homo_sapiens';
 
 $registry->load_all($registry_file);
 
 my $vdba = $registry->get_DBAdaptor($species, 'variation');
 my $cdba = $registry->get_DBAdaptor($species, 'core');
-#$vdba->use_vcf(1);
 
 my $population_adaptor = $vdba->get_PopulationAdaptor;
 my $slice_adaptor = $cdba->get_SliceAdaptor;
 my $ldFeatureContainerAdaptor = $vdba->get_LDFeatureContainerAdaptor;
 my $variation_adaptor = $vdba->get_VariationAdaptor;
+my $vfa = $vdba->get_VariationFeatureAdaptor;
 
 #my $chr = 6;  #defining the region in chromosome 6
 #my $start = 25_837_556;
@@ -31,19 +31,27 @@ my $variation_adaptor = $vdba->get_VariationAdaptor;
 my $chr = 7;
 my $start =  48_902_556;
 my $end = 48_903_891;
+my $variant_name = 'rs611646';
+
+my $variant = $variation_adaptor->fetch_by_name($variant_name);
+print $variant->name, "\n";
+my $vfs = $variant->get_all_VariationFeatures;
+my $vf = $vfs->[0];
+
 my $population_name = '1000GENOMES:phase_3:ACB'; #we only want LD in this population
 my $population = $population_adaptor->fetch_by_name($population_name); #get population object from database
 
 
-print "fetch_by_Slice use VCF\n";
+#print "fetch_by_Slice use VCF\n";
 my $slice = $slice_adaptor->fetch_by_region('chromosome',$chr,$start,$end); #get slice of the region
 $ldFeatureContainerAdaptor->db->use_vcf(1);
+#$ldFeatureContainerAdaptor->db->include_failed_variations(1);
 #my $ldFeatureContainer = $ldFeatureContainerAdaptor->fetch_by_Slice($slice);
 my $ldFeatureContainer = $ldFeatureContainerAdaptor->fetch_by_Slice($slice, $population);
 
+$ldFeatureContainer = $ldFeatureContainerAdaptor->fetch_by_VariationFeature($vf, $population);
 
-
-print_container_content($ldFeatureContainer);
+#print_container_content($ldFeatureContainer);
 
 print "fetch_by_VariationFeatures use VCF\n";
 #High LD between variations rs114473994-rs6904823
