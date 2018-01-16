d4ec1a987b111a49b6460d0e62bd2c577fc22a4a
at7 <anja@ebi.ac.uk>
2018-01-16 16:49:43 +0000

    goat data checks

---
+++ production/goat/data_checks.pl
@@ -0,0 +1,152 @@
+use strict;
+use warnings;
+
+use Bio::EnsEMBL::IO::Parser::VCF4Tabix;
+use Bio::EnsEMBL::IO::Parser::VCF4;
+use FileHandle;
+use Array::Utils qw(:all);
+
+my $data_dir = '/hps/nobackup/production/ensembl/anja/release_92/goat/';
+
+my $files = {
+  IRCH => { origin => 'Iran',
+            genus => 'IRCH.genus_snps.CHIR1_0.20140928.vcf.gz',
+            population => 'IRCH.population_sites.CHIR1_0.20140307.vcf.gz' },
+
+  MOCH => { origin => 'Morocco',
+            genus => 'MOCH.genus_snps.CHIR1_0.20140928.vcf.gz',
+            population => 'MOCH.population_sites.CHIR1_0.20140307.vcf.gz' },
+
+  AUCH => { origin => 'Australia',
+            genus => 'AUCH.genus_snps.CHIR1_0.20140928.vcf.gz', },
+
+  AUFR => { origin => 'France',
+            genus => 'FRCH.genus_snps.CHIR1_0.20140928.vcf.gz', },
+
+  ITCH => { origin => 'Italy',
+            genus => 'ITCH.genus_snps.CHIR1_0.20140928.vcf.gz' },
+};
+
+
+&overlap_matrix;
+
+sub overlap_matrix {
+
+  my @sets = qw/AUCH AUFR ITCH IRCH MOCH/; 
+
+  my $variants = {};
+  foreach my $population (keys %$files) {
+    my $file = $files->{$population}->{'genus'};
+    my $fh = FileHandle->new("$data_dir/$population\_all_variants_genus.txt", 'r');
+    while (<$fh>) {
+      chomp;
+      my ($id, $chrom, $pos) = split/\t/;
+      $variants->{$population}->{$id} = 1;
+    }
+    $fh->close;
+  }
+
+  foreach my $p1 (@sets) {
+    foreach my $p2 (@sets) {
+      next if ($p1 eq $p2);
+      # get items from array @a that are not in array @b
+      my @a = keys %{$variants->{$p1}};
+      my @b = keys %{$variants->{$p2}};
+      my @minus = array_minus( @a, @b );
+      print STDERR "Variants in $p1 that are not in $p2: ", scalar @minus,"\n";
+      foreach my $id (@minus) {
+        print STDERR "    $id\n";
+      }
+    }
+  }
+}
+
+sub variant_set_overlap {
+
+  my $all_variants = {};
+  my $overlap_counts = {};
+  my $variant_counts = {};
+  my $id2file = {};
+  foreach my $population (keys %$files) {
+    foreach my $file_type (qw/genus population/) {
+      my $file = $files->{$population}->{$file_type};
+      next if (!$file);
+      print STDERR "$data_dir/$population\_all_variants_$file_type.txt\n";
+      my $fh = FileHandle->new("$data_dir/$population\_all_variants_$file_type.txt", 'r');
+      my $variant_count = {};
+      while (<$fh>) {
+        chomp;
+        my ($id, $chrom, $pos) = split/\t/;
+        $all_variants->{$id} = 1;
+        $variant_count->{$id} = 1;
+      }
+      $fh->close;
+      $variant_counts->{"$population\_$file_type"} = scalar keys %$variant_count;
+    }
+  }
+  foreach my $population (keys %$files) {
+    foreach my $file_type (qw/genus population/) {
+      my $file = $files->{$population}->{$file_type};
+      next if (!$file);
+      my $fh = FileHandle->new("$data_dir/$population\_all_variants_$file_type.txt", 'r');
+      my $variants = {};
+      while (<$fh>) {
+        chomp;
+        my ($id, $chrom, $pos) = split/\t/;
+        $variants->{$id} = 1;
+      }
+      $fh->close;
+      my @a = keys %$all_variants;
+      my @b = keys %$variants;
+      my @intersection = intersect(@a, @b);
+      $overlap_counts->{"$population\_$file_type"} = scalar @intersection;
+    }
+  }
+
+  print STDERR "Count all variants ", scalar keys %$all_variants, "\n";
+  foreach my $file (keys %$variant_counts) {
+    my $count = $variant_counts->{$file};
+    my $overlap = $overlap_counts->{$file};
+    print STDERR "$file $count $overlap\n";
+  }
+
+}  
+
+sub print_gts {
+  foreach my $population (keys %$files) {
+    foreach my $file_type (qw/genus population/) {
+      my $file = $files->{$population}->{$file_type};
+      next if (!$file);
+      my $fh = FileHandle->new("$data_dir/$population\_all_GTs_$file_type.txt", 'w');
+      my $vcf_file = "$data_dir/$file";
+      my $parser = Bio::EnsEMBL::IO::Parser::VCF4Tabix->open($vcf_file);
+
+      while ($parser->next) {
+        my $gt_counts = {};
+        my $gts = $parser->get_samples_genotypes;
+        my @samples = @{$parser->get_samples};
+        foreach my $value (values %$gts) {
+          $gt_counts->{$value}++;
+        }
+        my @gt_frequencies = ();
+        foreach my $gt (keys %$gt_counts) {
+          my $count = $gt_counts->{$gt};
+          my $frequency = sprintf("%.4f", $count / scalar @samples);
+          push @gt_frequencies, "$gt:$frequency";
+        } 
+
+        my $reference = $parser->get_reference;
+        my @alternatives = @{$parser->get_alternatives};
+        my $allele_string = join('/', $reference, @alternatives);
+
+        my $seq_name = $parser->get_seqname;
+        my $start = $parser->get_start;
+        my @IDs = split(',', $parser->get_raw_IDs);
+        foreach my $id (@IDs) {
+          print $fh join("\t", $id, $seq_name, $start, $allele_string, @gt_frequencies), "\n";
+        }
+      }
+      $fh->close;
+    }
+  }
+}
