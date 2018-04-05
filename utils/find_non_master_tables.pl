2c5baa3c351eeab26f14b174d865767c586f7162
at7 <anja@ebi.ac.uk>
2018-04-05 11:05:39 +0100

    release 92 production work

--- utils/find_non_master_tables.pl
+++ utils/find_non_master_tables.pl
@@ -3,10 +3,12 @@ use warnings;
 
 use FileHandle;
 
-my $fh_all = FileHandle->new('/hps/nobackup/production/ensembl/anja/release_91/HC/gallus_gallus_91_5_all_tables', 'r');
-my $fh_master = FileHandle->new('/hps/nobackup/production/ensembl/anja/release_91/HC/variation_master_tables', 'r');
+my $dir = '/hps/nobackup/production/ensembl/anja/release_92/HC/';
 
-my $fh_non_master = FileHandle->new('/hps/nobackup/production/ensembl/anja/release_91/HC/gallus_gallus_master_tables', 'w');
+my $fh_all = FileHandle->new("$dir/capra_hircus_variation_92_1_all_tables", 'r');
+my $fh_master = FileHandle->new("$dir/master_schema_variation_92", 'r');
+
+my $fh_non_master = FileHandle->new("$dir/capra_hircus_variation_92_1_non_master", 'w');
 
 
 my $master_tables = {};
