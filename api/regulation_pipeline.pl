2cc4e9a2fc96bbb3a1659e8c1186cec293eb35e0
at7 <anja@ebi.ac.uk>
2018-06-22 15:36:32 +0100

    updates

--- api/regulation_pipeline.pl
+++ api/regulation_pipeline.pl
@@ -3,41 +3,26 @@ use Bio::EnsEMBL::Registry;
 
 my $registry = 'Bio::EnsEMBL::Registry';
 
-my $file = '/lustre/scratch110/ensembl/at7/release_85/ensembl.registry';
+my $file = '/hps/nobackup2/production/ensembl/anja/release_93/mouse/regulation_effect/ensembl.registry';
 
 $registry->load_all($file);
 
-my $vfa = $registry->get_adaptor('human', 'variation', 'variationfeature');
-my $sa = $registry->get_adaptor('human', 'core', 'slice');
-my $rfa = $registry->get_adaptor('human', 'funcgen', 'regulatoryfeature');
-my $mfa = $registry->get_adaptor('human', 'funcgen', 'motiffeature');
+my $sa = $registry->get_adaptor('mouse', 'core', 'slice');
+my $rfa = $registry->get_adaptor('mouse', 'funcgen', 'regulatoryfeature');
+my $mfa = $registry->get_adaptor('mouse', 'funcgen', 'motiffeature');
 
-my $ga = $registry->get_adaptor('human', 'core', 'gene');
+my $ga = $registry->get_adaptor('mouse', 'core', 'gene');
 
-my @genes = @{ $ga->fetch_all };
-foreach my $gene (@genes) {
-  print STDERR $gene->stable_id, "\n";
-}
-
-#my $slices = $sa->fetch_all('toplevel', undef, 0, 1);
-
-
-#my @feature_ids = ();
-#foreach my $slice (@$slices) {
-
-#  my @mfs = @{$mfa->fetch_all_by_Slice($slice)};
-#  print scalar @mfs, "\n";
-
-
-
+my $slices = $sa->fetch_all('toplevel', undef, 0, 1);
+foreach my $slice (@$slices) {
+  print STDERR $slice->seq_region_name, "\n";
+  my @mfs = @{$mfa->fetch_all_by_Slice($slice)};
+  print STDERR "MFs ", scalar @mfs, "\n";
 #  my $rfs = $rfa->fetch_all_by_Slice($slice);
-#  foreach my $rf (@$rfs) {
-#    push @feature_ids, { feature_id => $rf->stable_id,
-#                         feature_type => 'regulatory_feature',
-#                         species => 'homo_sapiens', };
+#  if ($rfs) {
+#    print STDERR "RFs ", scalar @$rfs, "\n";
 #  }
-
-#}
+}
 
 
 
