a81c3674eb51628b69628f94c733d435ba8a9ba4
at7 <anja@ebi.ac.uk>
2018-10-01 14:20:07 +0100

    updates

--- production/finish_dumps.pl
+++ production/finish_dumps.pl
@@ -8,9 +8,9 @@ use Bio::EnsEMBL::Registry;
 my $registry = 'Bio::EnsEMBL::Registry';
 
 #my $data_dump_dir = '/hps/nobackup/production/ensembl/anja/release_92/dumps/grch38/';
-my $data_dump_dir = '/hps/nobackup2/production/ensembl/anja/release_93/human/dumps/';
+my $data_dump_dir = '/hps/nobackup2/production/ensembl/anja/release_94/dumps/';
 
-my $registry_file = '/hps/nobackup/production/ensembl/anja/release_92/dumps/grch38/ensembl.registry';
+#my $registry_file = '/hps/nobackup/production/ensembl/anja/release_92/dumps/grch38/ensembl.registry';
 #my $registry_file = '/hps/nobackup/production/ensembl/anja/release_90/dumps_human/ensembl.registry';
 
 
@@ -25,7 +25,7 @@ $all_species->{'homo_sapiens'} = 1;
 #  next unless ($species_name eq 'homo_sapiens');
 #  $all_species->{$species_name} = 1;
 #}
-tabix_vcf_files($data_dump_dir, $all_species);
+#tabix_vcf_files($data_dump_dir, $all_species);
 compute_checksums($data_dump_dir, $all_species);
 
 sub tabix_vcf_files {
@@ -63,7 +63,7 @@ sub tabix_vcf_files {
 
 sub compute_checksums {
   my ($data_dir, $all_species) = @_;
-  foreach my $file_type (qw/vcf/) {
+  foreach my $file_type (qw/vcf gvf/) {
 #  foreach my $file_type (qw/vcf/) {
 
     foreach my $species (keys %$all_species) {
