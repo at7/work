60707a637c637378887be4869dfbf01ac6ca6747
at7 <anja@ebi.ac.uk>
2018-11-30 14:43:39 +0000

    release/95

--- utils/find_non_master_tables.pl
+++ utils/find_non_master_tables.pl
@@ -3,12 +3,10 @@ use warnings;
 
 use FileHandle;
 
-my $dir = '/hps/nobackup/production/ensembl/anja/release_92/HC/';
+my $dir = '/hps/nobackup2/production/ensembl/anja/release_95/HC/';
 
-my $fh_all = FileHandle->new("$dir/capra_hircus_variation_92_1_all_tables", 'r');
-my $fh_master = FileHandle->new("$dir/master_schema_variation_92", 'r');
-
-my $fh_non_master = FileHandle->new("$dir/capra_hircus_variation_92_1_non_master", 'w');
+my $fh_all = FileHandle->new("$dir/human_38_all_tables", 'r');
+my $fh_master = FileHandle->new("$dir/master_schema_variation_95", 'r');
 
 
 my $master_tables = {};
@@ -17,7 +15,6 @@ while (<$fh_master>) {
   $master_tables->{$_} = 1;
 }
 $fh_master->close();
-
 
 
 my $all_tables = {};
@@ -31,9 +28,13 @@ $fh_all->close();
 my $non_master = {};
 
 foreach my $table (keys %$all_tables) {
-  if (!($table =~ /^MTMP/ || $master_tables->{$table})) {
+  if (!($master_tables->{$table})) {
     $non_master->{$table} = 1;
   }
 }
 
-print join(' ', keys %$non_master), "\n";
+my $fh = FileHandle->new("$dir/non_master_schema_variation_95", 'w');
+
+print $fh join(' ', keys %$non_master), "\n";
+
+$fh->close;
