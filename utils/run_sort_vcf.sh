bbfa944a4e0c7386a762a649da2923111af5de07
at7 <anja@ebi.ac.uk>
2018-11-30 14:42:58 +0000

    release 95

--- utils/run_sort_vcf.sh
+++ utils/run_sort_vcf.sh
@@ -1,3 +1,3 @@
-vcf_file=/hps/nobackup2/production/ensembl/anja/release_94/human/dumps/vcf/homo_sapiens/1000GENOMES-phase_3.vcf
-vcf_file_gz=/hps/nobackup2/production/ensembl/anja/release_94/human/dumps/vcf/homo_sapiens/1000GENOMES-phase_3.vcf.gz
+vcf_file=/hps/nobackup2/production/ensembl/anja/release_95/human/dumps/vcf/homo_sapiens/1000GENOMES-phase_3.vcf
+vcf_file_gz=/hps/nobackup2/production/ensembl/anja/release_95/human/dumps/vcf/homo_sapiens/1000GENOMES-phase_3.vcf.gz
 vcf-sort < $vcf_file | bgzip > $vcf_file_gz
