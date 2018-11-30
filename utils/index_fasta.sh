dbfb14deab591a1a69203b9c0dd69ff79fc5cc45
at7 <anja@ebi.ac.uk>
2018-11-30 14:44:49 +0000

    cat

--- utils/index_fasta.sh
+++ utils/index_fasta.sh
@@ -1,7 +1,7 @@
-tmpdir=/hps/nobackup2/production/ensembl/anja/release_94/chicken/remapping/new_assembly/
+tmp_dir=/hps/nobackup2/production/ensembl/anja/release_95/cat/remapping/new_assembly/
 bsub \
 -J bwa_index \
 -o ${tmpdir}bwa_index.out \
 -e ${tmpdir}bwa_index.err \
 -M4500 -R"select[mem>4500] rusage[mem=4500]" \
-bwa index -a bwtsw ${tmpdir}Gallus_gallus.Gallus_gallus-5.0.dna_sm.toplevel.fa
+bwa index -a bwtsw ${tmpdir}Felis_catus.Felis_catus_9.0.dna_sm.toplevel.fa
