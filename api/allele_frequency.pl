c15ce392f786c3841fd24a5fd41e40b34309d42f
at7 <anja@ebi.ac.uk>
2019-09-10 11:40:14 +0100

    updates

--- api/allele_frequency.pl
+++ api/allele_frequency.pl
@@ -1,19 +1,20 @@
 use strict;
 use warnings;
 use Bio::EnsEMBL::Registry;
-
+use Bio::EnsEMBL::Variation::Population;
 my $registry = 'Bio::EnsEMBL::Registry';
 
 $registry->load_registry_from_db(
   -host => 'ensembldb.ensembl.org',
   -user => 'anonymous',
+  -db_version => 97,
 );
 
 my $va = $registry->get_adaptor('human', 'variation', 'variation');
-$va->db->use_vcf(1);
+$va->db->use_vcf(2);
 my $variation = $va->fetch_by_name('rs2359294');
 
-my @alleles = @{$variation->get_all_Alleles};
+my @alleles = @{$variation->get_all_Alleles()};
 
 foreach my $a (@alleles){
 	my $freq = $a->frequency;
