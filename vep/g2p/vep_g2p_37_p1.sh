0198a1818700a37c36a8d42a87ac4e8d772ad3eb
at7 <anja@ebi.ac.uk>
2019-09-10 11:41:16 +0100

    updates

--- vep/g2p/vep_g2p_37_p1.sh
+++ vep/g2p/vep_g2p_37_p1.sh
@@ -1,12 +1,12 @@
 vep_data=/hps/nobackup2/production/ensembl/anja/
 perl $HOME/bin/ensembl-vep/vep \
---cache --cache_version 90 \
+--cache --cache_version 97 \
 --assembly GRCh37 \
---port 3337 \
---dir /hps/nobackup2/production/ensembl/anja/vep/ \
+--offline \
+--dir /nfs/production/panda/ensembl/variation/data/VEP/tabixconverted/ \
 --input_file ${vep_data}/vep_data/input/grch37/P1.vcf.gz \
 --output_file ${vep_data}/vep_data/output/grch37/P1.out \
 --force_overwrite \
 --individual 'P1' \
-#--dir_plugins $HOME/bin/VEP_plugins \
-#--plugin G2P,file="${vep_data}/G2P/spring2019/Intellectual_disability.tsv" \
+--dir_plugins $HOME/bin/VEP_plugins \
+--plugin G2P,file="${vep_data}/G2P/spring2019/Intellectual_disability.tsv",af_from_vcf=1 \
--- vep/g2p/vep_g2p_97_37_test_input.sh
+++ vep/g2p/vep_g2p_97_37_test_input.sh
@@ -3,12 +3,12 @@ perl $HOME/bin/ensembl-vep/vep \
 --cache --cache_version 97 \
 --db_version 97 \
 --assembly GRCh37 \
---offline \
 --dir /nfs/production/panda/ensembl/variation/data/VEP/tabixconverted/ \
 --input_file ${vep_data}/vep_data/input/grch37/P1.vcf \
 --output_file ${vep_data}/vep_data/output/grch37/P1_10072019.out \
 --force_overwrite \
 --individual ALL \
+--port 3337 \
 --dir_plugins $HOME/bin/VEP_plugins \
---plugin G2P,${vep_data}/G2P/spring2019/DDG2P_25_4_2019.csv \
+--plugin G2P,file="${vep_data}/G2P/spring2019/DDG2P_25_4_2019.csv",af_from_vcf=1 \
 #--plugin G2P,file="${vep_data}/G2P/spring2019/DDG2P_25_4_2019.csv",af_from_vcf=1 \
